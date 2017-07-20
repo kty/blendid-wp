43cd9c655f8a71284e231fc8948230c1d7c296f0
alexanderalmstrom <alexander.almstrom@bizkithavas.se>
2017-07-20 14:50:45 +0200

    Hide and show comment forms and reply buttons

--- src/javascripts/components/comments.js
+++ src/javascripts/components/comments.js
@@ -1,9 +1,28 @@
 $(function() {
   $('.comment-reply').on('click', function(event) {
+    event.preventDefault();
+
+    let reply_btn = $(this)
     let comment = $(this).parent('.blog-comment')
     let form = comment.children('.comment-form')
 
-    $('.blog-comment .comment-form').removeClass('d-block')
-    form.addClass('d-block')
+    $('.blog-comment .comment-reply').removeClass('hide')
+    $('.blog-comment .comment-form').removeClass('show')
+
+    reply_btn.addClass('hide')
+    form.addClass('show')
+  })
+
+  $('.comment-cancel').on('click', function(event) {
+    event.preventDefault();
+
+    let comment = $(this).parents('.blog-comment')
+    let form = comment.children('.comment-form')
+    let reply_btn = comment.find('.comment-reply')
+
+    $('.blog-comment .comment-form').removeClass('show')
+
+    reply_btn.removeClass('hide')
+    form.removeClass('show')
   })
 });
